7d3d66da98c42d57828b509b50d43ba687c41f49
netizen-om <borisagarom@gmail.com>
2025-01-17 13:16:48 +0530

    add login and signup routes

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function loginPage() {
+    
+    return (
+        <div className="min-h-screen flex justify-center items-center text-3xl ">
+            <h1>Login</h1>
+        </div>
+    )
+
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function singupPage(){ 
+    return (
+        <div className="min-h-screen flex justify-center items-center text-3xl ">
+            <h1>Signup</h1>
+        </div>
+    )
+}
